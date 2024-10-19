38ca5ecb970f7ad1f674557fb96e7e0cd0644e50
eduardog949 <eduardo.g949@outlook.com>
2024-10-19 11:19:11 -0700

    simple starter javascript

---
+++ index.js
@@ -0,0 +1,15 @@
+let count = 0;
+console.log(count);
+let totalCount = count + 1;
+console.log(totalCount);
+
+let sum1 = 40;
+let sum2 = 50;
+let sum3 = 80;
+
+function totalAdd(){
+    let totalSum = sum1 + sum2 + sum3;
+    console.log(totalSum);
+}
+totalAdd();
+
